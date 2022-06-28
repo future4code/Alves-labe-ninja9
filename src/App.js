e0ffcc744fcafeb4c416dd276491029ad1d9018e
João Gabriel Colodetti do Carmo <jgcolodetti13@hotmail.com>
2022-06-28 17:25:03 -0300

    loja e switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import VizualizarPost from './components/VizualizarPost/VisualizarPost.js'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Main = styled.div`
+display: flex;
+`
+const Container = styled.div`
+display: flex;
+`
+export default class App extends Component {
+  render() {
+    return (
+      <Main>
+        <VizualizarPost>
+        </VizualizarPost>
+      </Main>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/VizualizarPost/VisualizarPost.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const Busca = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 80px;
+`
+
+const Card = styled.div`
+  display: flex;
+  margin: 50px 0px 50px 65px;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`
+
+const Trabalho = styled.div`
+  margin: 30px;
+  border: 1px solid black;
+  padding: 20px 10px;
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px RGB(128, 126, 126);
+  text-align: center;
+  width: 14vw;
+`
+
+const Title = styled.h3`
+margin: 0;
+overflow: hidden;
+text-overflow: ellipsis;
+opacity: 0.7;
+:hover{
+    cursor: pointer;
+    opacity: 1;
+}
+`
+
+export default class VisualizarPost extends Component {
+
+  state = {
+
+    nome: '',
+    valorMinimo: '',
+    valorMaximo: '',
+    ordenacao: 'crecente',
+    listaDePost: [],
+
+    telas: true,
+
+  }
+
+  componentDidMount() {
+    this.getAllJobs()
+  }
+
+  pesquisaNome = (event) => {
+    this.setState({ nome: event.target.value })
+  }
+
+  pesquisaPrecoMinimo = (event) => {
+    this.setState({ valorMaximo: event.target.value })
+  }
+
+  pesquisaPrecoMaximo = (event) => {
+    this.setState({ valorMaximo: event.target.value })
+  }
+
+  ordenacao = (event) => {
+    this.setState({ ordenacao: event.target.value })
+  }
+
+  getAllJobs = () => {
+
+    axios.get(`https://labeninjas.herokuapp.com/jobs`, {
+      headers: {
+        'Authorization': 'e2190c39-7930-4db4-870b-bed0e5e4b88e'
+      }
+    }).then(resposta => {
+      this.setState({ listaDePost: resposta.data.jobs })
+    }).catch(erro => {
+      console.log(erro.response.data.message)
+    })
+  }
+
+  render() {
+
+    const novaLista = this.state.listaDePost.filter(item => {
+      return item.title.toLowerCase().includes(this.state.nome.toLowerCase())
+    }).filter(item => {
+      return this.state.valorMinimo === '' || item.price >= this.state.valorMinimo
+    }).filter(item => {
+      return this.state.valorMaximo === '' || item.price <= this.state.valorMaximo
+    }).sort((item1, item2) => {
+      switch (this.state.ordenacao) {
+        case "crecente":
+          return item1.price - item2.price
+        case "decrecente":
+          return item2.price - item1.price
+        case 'titulo':
+          return item1.title.localeCompare(item2.title)
+        case 'prazo':
+          return new Date(item1.dueDate).getTime() - new Date(item2.dueDate).getTime();
+        default:
+          break
+      }
+    }).map((Card) => {
+      return <Trabalho key={Card.id}>
+        <Title>{Card.title}</Title>
+        <p>{Card.dueDate} por {Card.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+        <button>Ver detalhes</button>
+        <button>adicionar ao carrinho</button>
+      </Trabalho>
+    })
+
+    let trocaTela
+    switch (this.state.telas) {
+
+      case true:
+        trocaTela = <div>
+
+          <Busca>
+            <input onChange={this.pesquisaNome} type="text" placeholder="Nome"></input>
+            <input onChange={this.pesquisaPrecoMinimo} type="number" placeholder="R$ Valor Mínimo"></input>
+            <input onChange={this.pesquisaPrecoMaximo} type="number" placeholder="R$ Valor Máximo"></input>
+            <select onChange={this.ordenacao}>
+              <option value="crecente">Preço Crecente</option>
+              <option value="decrecente">Preço Decrecente</option>
+              <option value="titulo">Titulo</option>
+              <option value="prazo">Prazo</option>
+            </select>
+          </Busca>
+
+          <Card>
+            {novaLista}
+          </Card>
+
+        </div>
+        break;
+
+      case false:
+        trocaTela = <div>
+
+        </div>
+        break;
+
+      default:
+        break;
+    }
+
+    return (
+      <div>
+        {trocaTela}
+      </div>
+    )
+  }
+}
